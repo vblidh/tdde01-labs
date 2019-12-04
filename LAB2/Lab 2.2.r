4189478ed505eca523537e78c5f32137211baa1e
Viktor Blidh <vikbl327@student.liu.se>
2019-12-04 10:20:30 +0100

    Almost done with assignment 2

--- LAB2/Lab 2.2.r
+++ LAB2/Lab 2.2.r
@@ -1,10 +1,15 @@
+library("e1071")
+library("MASS")
 library("tree")
 library("readxl")
+library("ggplot2")
 set.seed(12345)
 RNGversion('3.5.1')
-setwd("C:\\Users\\Victor\\Documents\\R Projects\\tdde01-labs\\LAB2")
+#setwd("C:\\Users\\Victor\\Documents\\R Projects\\tdde01-labs\\LAB2")
 
-data = data.frame(read_excel("creditscoring.xls"))
+data2 = read_excel("creditscoring.xls")
+data = read.csv("creditscoring.csv")
+
 n=dim(data)[1]
 set.seed(12345)
 id=sample(1:n, floor(n*0.5))
@@ -16,8 +21,100 @@ valid=data[id2,]
 id3=setdiff(id1,id2)
 test=data[id3,] 
 
-tree.deviance = tree(good_bad~., data=train, split = c("deviance"))
+tree_dev = tree(good_bad~., data=train, split = c("deviance"))
+tree_dev.pred.train = predict(tree_dev, newdata=train, type="class")
+tree_dev.pred.test = predict(tree_dev, newdata = test, type="class")
+tree_dev.cm.train = table(train$good_bad, tree_dev.pred.train)
+tree_dev.cm.test = table(test$good_bad, tree_dev.pred.test)
+tree_dev.me.train = 1 - sum(diag(tree_dev.cm.train))/(sum(tree_dev.cm.train))
+tree_dev.me.test = 1 - sum(diag(tree_dev.cm.test))/(sum(tree_dev.cm.test))
 
-tree.gini = tree(good_bad~., data=train, split = c("gini"))
 
-tree.deviance.pred = predict(tree.deviance, type="class")
+tree_gini = tree(good_bad~., data=train, split = c("gini"))
+tree_gini.pred.train = predict(tree_gini, newdata=train, type="class")
+tree_gini.pred.test = predict(tree_gini, newdata=test, type="class")
+tree_gini.cm.train = table(train$good_bad, tree_gini.pred.train)
+tree_gini.cm.test = table(test$good_bad, tree_gini.pred.test)
+tree_gini.me.train = 1 - sum(diag(tree_gini.cm.train))/(sum(tree_gini.cm.train))
+tree_gini.me.test = 1 - sum(diag(tree_gini.cm.test))/(sum(tree_gini.cm.test))
+
+#3
+fit = tree(good_bad~., data=train)
+fit$frame
+trainScore=rep(0,15)
+testScore=rep(0,15)
+for(i in 2:15) {
+  prunedTree=prune.tree(fit,best=i)
+  pred=predict(prunedTree, newdata=valid,
+               type="tree")
+  trainScore[i]=deviance(prunedTree)
+  testScore[i]=deviance(pred)
+}
+plot(2:15, trainScore[2:15], type="b", col="red",
+     ylim=c(250,600))
+points(2:15, testScore[2:15], type="b", col="blue")
+
+finalTree=prune.tree(fit, best=4)
+finalTree.pred.class=predict(finalTree, newdata=test,
+             type="class")
+finalTree.cm = table(test$good_bad,Yfit)
+finalTree.me = 1 - sum(diag(finalTree.cm))/(sum(finalTree.cm))
+
+#4
+naiveBayes.fit = naiveBayes(good_bad~., data=train)
+naiveBayes.pred.train = predict(naiveBayes.fit, newdata=train)
+naiveBayes.pred.test = predict(naiveBayes.fit, newdata=test)
+naiveBayes.cm.train = table(naiveBayes.pred.train, train$good_bad)
+naiveBayes.cm.test = table(naiveBayes.pred.test, test$good_bad)
+naiveBayes.me.train = 
+  1 - sum(diag(naiveBayes.cm.train))/(sum(naiveBayes.cm.train))
+naiveBayes.me.test = 
+  1 - sum(diag(naiveBayes.cm.test))/(sum(naiveBayes.cm.test))
+
+#5
+
+Compute_TPR_FPR = function(prediction){
+  pi_vector = seq(0.05, 0.95, 0.05)
+  TPR_vector = rep(0, length(pi_vector))
+  FPR_vector = rep(0, length(pi_vector))
+  N.tot = length(which(test$good_bad == "bad"))
+  P.tot = length(which(test$good_bad == "good"))
+  for (i in 1:length(pi_vector)){
+    classification = ifelse(prediction[,2] > pi_vector[i], "good", "bad")
+    
+    TP = length(which(classification=="good" & test$good_bad == "good"))
+    FP = length(which(classification=="good" & test$good_bad == "bad"))
+    
+    TPR = TP/P.tot
+    FPR = FP/N.tot
+
+    TPR_vector[i] = TPR
+    FPR_vector[i] = FPR
+    
+  }
+  data.frame("Pi value" = pi_vector,"TPR" = TPR_vector, 
+             "FPR" = FPR_vector)
+}
+
+naiveBayes.pred.test.numeric = predict(naiveBayes.fit, newdata = test, type="raw")
+finalTree.pred.numeric = predict(finalTree, newdata = test)
+
+tree_TPR_FPR = Compute_TPR_FPR(finalTree.pred.numeric)
+bayes_TPR_FPR = Compute_TPR_FPR(naiveBayes.pred.test.numeric)
+
+ggplot(data = NULL, aes(col="classifier")) + 
+  geom_point(data = tree_TPR_FPR, aes(x=FPR, y=TPR, col="Tree")) + 
+  geom_line(data = tree_TPR_FPR, aes(x=FPR, y=TPR, col="Tree")) + 
+  geom_point(data = bayes_TPR_FPR, aes(x=FPR, y=TPR, col="Bayes")) + 
+  geom_line(data = bayes_TPR_FPR, aes(x=FPR, y=TPR, col="Bayes"))
+
+
+#6
+
+naiveBayes.pred.train.numeric = 
+  predict(naiveBayes.fit, newdata = train, type="raw")
+L = matrix(c(0,10,1,0),nrow = 2, ncol = 2)
+naiveBayes.pred.train.loss = 
+  ifelse(naiveBayes.pred.train.numeric[,2]/naiveBayes.pred.train.numeric[1,]
+         > (L[2,1]/L[1,2]), "good", "bad")
+
